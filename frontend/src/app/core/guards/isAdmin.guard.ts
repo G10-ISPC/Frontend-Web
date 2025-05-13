78f676bf45d4684de20a7fed8e204e5139b9ec72
Mica <micajuarez26@hotmail.com>
2025-05-13 17:38:59 -0300

    proteccion de rutas

---
+++ frontend/src/app/core/guards/isAdmin.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LogService } from '../services/log.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsAdminGuard implements CanActivate {
+
+  constructor(private logService: LogService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.logService.isAdmin.pipe(
+      map(isAdmin => {
+        if (isAdmin) {
+          return true;
+        } else {
+          this.router.navigate(['/home']);
+          alert('Acceso denegado: solo para administradores');
+          return false;
+        }
+      })
+    );
+  }
+}
