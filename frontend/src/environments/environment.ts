ecb18f067c96504baf4d64a9993546eb220cd9ac
carla <arevalocarlaelizabeth@gmail.com>
2025-07-30 23:01:07 -0300

    agregado de corrección de url back

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ricco-backend.onrender.com' // o tu URL real de producción
+  apiUrl: 'https://ricco-backend.onrender.com/api' // o tu URL real de producción
 };
